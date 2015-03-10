2dfe7a19ab1bf41f35aa58b2c615e851e6df0473
Misha Moroshko <michael.moroshko@gmail.com>
2015-03-10 21:39:25 +1100

    Add tests

--- jest-script-preprocessor.js
+++
@@ -1,8 +0,0 @@
-var babelJest = require("babel-jest");
-
-module.exports = {
-  process: function(src, filename) {
-    return babelJest.process(src, filename)
-      .replace(/^require.*\.less.*;$/gm, ''); // Hack. See: http://stackoverflow.com/q/28870296/247243
-  }
-};
--- src/tests/Autosuggest.js
+++ src/tests/Autosuggest.js
@@ -5,13 +5,51 @@ jest.dontMock('../Autosuggest.js');
 var React = require('react/addons');
 var Autosuggest = require('../Autosuggest.js');
 var TestUtils = React.addons.TestUtils;
+var suburbs = ['Cheltenham', 'Mill Park', 'Mordialloc', 'Nunawading'];
+var autosuggest, input, suggestions;
+
+function getSuburbs(input, callback) {
+  var regex = new RegExp('^' + input, 'i');
+
+  callback(null, suburbs.filter(function(suburb) {
+    return regex.test(suburb);
+  }));
+}
 
 describe('Autosuggest', function() {
-  it('should render content', function() {
-    var autosuggest = TestUtils.renderIntoDocument(<Autosuggest />);
-    var autosuggestWrapper =
-      TestUtils.findRenderedDOMComponentWithClass(autosuggest, 'react-autosuggest');
+  describe('Basics', function() {
+    beforeEach(function() {
+      autosuggest = TestUtils.renderIntoDocument(
+        <Autosuggest initialValue="my value" suggestions={getSuburbs} />
+      );
+      input = TestUtils.findRenderedDOMComponentWithTag(autosuggest, 'input');
+    });
 
-    expect(autosuggestWrapper.getDOMNode().textContent).toEqual('Hello World');
+    it('should set initial value', function() {
+      expect(input.getDOMNode().value).toBe('my value');
+    });
+
+    it('should not show suggestions by default', function() {
+      suggestions = TestUtils.scryRenderedDOMComponentsWithClass(autosuggest, 'react-autosuggest__suggestion');
+      expect(suggestions.length).toBe(0);
+    });
+
+    it('should show suggestions when matches exist', function() {
+      React.addons.TestUtils.Simulate.change(input, { target: { value: 'm' } });
+      suggestions = TestUtils.scryRenderedDOMComponentsWithClass(autosuggest, 'react-autosuggest__suggestion');
+      expect(suggestions.length).toBe(2);
+    });
+
+    it('should show suggestions when case insensitive matches exist', function() {
+      React.addons.TestUtils.Simulate.change(input, { target: { value: 'NUNA' } });
+      suggestions = TestUtils.scryRenderedDOMComponentsWithClass(autosuggest, 'react-autosuggest__suggestion');
+      expect(suggestions.length).toBe(1);
+    });
+
+    it('should show not suggestions when no matches exist', function() {
+      React.addons.TestUtils.Simulate.change(input, { target: { value: 'a' } });
+      suggestions = TestUtils.scryRenderedDOMComponentsWithClass(autosuggest, 'react-autosuggest__suggestion');
+      expect(suggestions.length).toBe(0);
+    });
   });
 });
