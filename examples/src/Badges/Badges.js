ad3747bc345884868c77a4057d3a5ca26402397a
Misha Moroshko <michael.moroshko@gmail.com>
2015-05-18 21:12:09 +1000

    Fix badge URLs

--- examples/src/Badges/Badges.js
+++ examples/src/Badges/Badges.js
@@ -9,13 +9,13 @@ export default class Badges extends Component { // eslint-disable-line no-shadow
     return (
       <div className="badges">
         <a href="//codeship.com/projects/67868" target="_blank">
-          <img src="//img.shields.io/codeship/41810250-aa07-0132-fbf4-4e62e8945e03.svg" />
+          <img src="//img.shields.io/codeship/41810250-aa07-0132-fbf4-4e62e8945e03/master.svg" />
         </a>
         <a href="//npmjs.org/package/react-autosuggest" target="_blank">
           <img src="//img.shields.io/npm/v/react-autosuggest.svg" />
         </a>
         <a href="//npmjs.org/package/react-autosuggest" target="_blank">
-          <img src="https://img.shields.io/npm/dm/react-autosuggest.svg" />
+          <img src="//img.shields.io/npm/dm/react-autosuggest.svg" />
         </a>
       </div>
     );
