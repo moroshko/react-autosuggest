ea477432dfb052133ccc8861a3daff8c06d4f2f5
Cauê Alves <cauealveseng@gmail.com>
2016-06-10 15:28:43 -0300

    Open a url after create a server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
+var openUrl = require('openurl');
+
 var config = require('./webpack.dev.config');
 var host = process.env.NODE_HOST || 'localhost';
 var port = process.env.NODE_PORT || 3000;
@@ -9,7 +11,12 @@ new WebpackDevServer(webpack(config), {
 }).listen(port, host, function(error) {
   if (error) {
     console.error(error); // eslint-disable-line no-console
-  } else {
-    console.log(`Demo is ready at http://${host}:${port}/demo/dist/index.html`); // eslint-disable-line no-console
+    process.exit(1);
   }
+
+  var url = `http://${host}:${port}/demo/dist/index.html`;
+
+  console.log('Demo is ready at ' + url); // eslint-disable-line no-console
+
+  openUrl.open(url);
 });
