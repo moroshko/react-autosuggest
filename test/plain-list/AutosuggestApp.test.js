04b0c3f7973a29bbca1949c09bfddc0de5f9afe5
Thibaud Colas <thibaudcolas@gmail.com>
2016-06-25 16:03:15 +0300

    Add tests for alwaysRenderSuggestions behavior when selecting a suggestion

--- test/plain-list/AutosuggestApp.test.js
+++ test/plain-list/AutosuggestApp.test.js
@@ -502,6 +502,10 @@ describe('Default Autosuggest', () => {
       setAlwaysRenderSuggestions(false, done);
     });
 
+    it('should render suggestions initially', () => {
+      expectSuggestions(['Elm']);
+    });
+
     it('should render suggestions when input is focused', () => {
       focusAndSetInputValue('e');
       expectSuggestions(['Elm']);
@@ -522,6 +526,56 @@ describe('Default Autosuggest', () => {
       focusAndSetInputValue('');
       blurInput();
       expectSuggestions(['Elm']);
+    });
+
+    describe('when pressing Down', () => {
+      beforeEach(() => {
+        focusAndSetInputValue('p');
+      });
+
+      it('should focus on the first suggestion', () => {
+        clickDown();
+        expectFocusedSuggestion('Perl');
+      });
+    });
+
+    describe('when pressing Up', () => {
+      beforeEach(() => {
+        focusAndSetInputValue('p');
+      });
+
+      it('should focus on the last suggestion', () => {
+        clickUp();
+        expectFocusedSuggestion('Python');
+      });
+    });
+
+    describe('when pressing Enter', () => {
+      beforeEach(() => {
+        focusAndSetInputValue('p');
+      });
+
+      it('should not hide suggestions if there is a focused suggestion', () => {
+        clickDown();
+        clickEnter();
+        expectSuggestions(['Perl']);
+      });
+
+      it('should not hide suggestions if there is no focused suggestion', () => {
+        clickEnter();
+        expectSuggestions(['Perl', 'PHP', 'Python']);
+      });
+    });
+
+    describe('when suggestion is clicked', () => {
+      beforeEach(() => {
+        focusAndSetInputValue('p');
+        clickSuggestion(1);
+      });
+
+      it('should not hide suggestions', () => {
+        expectSuggestions(['PHP']);
+      });
     });
   });
 
