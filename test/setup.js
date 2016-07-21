655458c16eea13e9f61c8a0add1f06c283768d97
Misha Moroshko <michael.moroshko@gmail.com>
2016-07-21 21:49:24 +1000

    Refactor tests

---
+++ test/setup.js
@@ -0,0 +1,5 @@
+import jsdom from 'jsdom';
+
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = global.document.defaultView;
+global.navigator = global.window.navigator;
