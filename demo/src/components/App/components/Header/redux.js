1ca08b3f6353fba798bc701f52b6762e393dca5b
Misha Moroshko <michael.moroshko@gmail.com>
2016-09-06 16:15:41 +1000

    Add scrollable container example to demo (#239)

--- demo/src/components/App/components/Header/redux.js
+++ demo/src/components/App/components/Header/redux.js
@@ -3,7 +3,7 @@ import fetch from 'isomorphic-fetch';
 const UPDATE_STARGAZERS = 'UPDATE_STARGAZERS';
 
 const initialState = {
-  stargazers: '1102'
+  stargazers: '1140'
 };
 
 export function loadStargazers() {
