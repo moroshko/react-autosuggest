3dc869c6506fc3ed89866e77e119f76813d9d00c
Misha Moroshko <michael.moroshko@gmail.com>
2016-12-15 22:03:14 -0800

    Upgrade deps

--- demo/src/components/App/components/Header/redux.js
+++ demo/src/components/App/components/Header/redux.js
@@ -3,7 +3,7 @@ import fetch from 'isomorphic-fetch';
 const UPDATE_STARGAZERS = 'UPDATE_STARGAZERS';
 
 const initialState = {
-  stargazers: '1416'
+  stargazers: '1419'
 };
 
 export const loadStargazers = () => dispatch =>
