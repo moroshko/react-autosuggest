fe3cae48ffd43bd1afc1219ede923427f1791966
Taylor Smith <twsmith89@gmail.com>
2017-04-19 17:14:51 -0500

    React 15.5.0 compatibility (#352)

--- test/multi-section/AutosuggestApp.test.js
+++ test/multi-section/AutosuggestApp.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import TestUtils from 'react-addons-test-utils';
+import TestUtils from 'react-dom/test-utils';
 import { expect } from 'chai';
 import sinon from 'sinon';
 import {
