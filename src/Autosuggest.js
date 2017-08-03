3ddbe76ed31b3cb850442ddd0845bdc17cfae277
Daniel Korger <dkorger@ebay.com>
2017-08-03 16:43:50 +0200

    fixes sticky selection list for touch devices

--- src/Autosuggest.js
+++ src/Autosuggest.js
@@ -7,7 +7,9 @@ import { defaultTheme, mapToAutowhateverTheme } from './theme';
 const alwaysTrue = () => true;
 const defaultShouldRenderSuggestions = value => value.trim().length > 0;
 const defaultRenderSuggestionsContainer = ({ containerProps, children }) =>
-  <div {...containerProps}>{children}</div>;
+  <div {...containerProps}>
+    {children}
+  </div>;
 
 export default class Autosuggest extends Component {
   static propTypes = {
@@ -250,9 +252,8 @@ export default class Autosuggest extends Component {
     );
 
     return {
-      sectionIndex: typeof sectionIndex === 'string'
-        ? parseInt(sectionIndex, 10)
-        : null,
+      sectionIndex:
+        typeof sectionIndex === 'string' ? parseInt(sectionIndex, 10) : null,
       suggestionIndex: parseInt(suggestionIndex, 10)
     };
   }
@@ -326,6 +327,10 @@ export default class Autosuggest extends Component {
 
   onSuggestionMouseDown = () => {
     this.justSelectedSuggestion = true;
+  };
+
+  onSuggestionTouchEnd = event => {
+    this.justSelectedSuggestion = false;
   };
 
   onSuggestionsClearRequested = () => {
@@ -418,6 +423,7 @@ export default class Autosuggest extends Component {
       onMouseLeave: this.resetHighlightedSuggestionOnMouseLeave,
       onMouseDown: this.onSuggestionMouseDown,
       onTouchStart: this.onSuggestionMouseDown, // Because on iOS `onMouseDown` is not triggered
+      onTouchEnd: this.onSuggestionTouchEnd,
       onClick: this.onSuggestionClick
     };
   };
@@ -550,9 +556,8 @@ export default class Autosuggest extends Component {
                 // valueBeforeUpDown can be null if, for example, user
                 // hovers on the first suggestion and then pressed Up.
                 // If that happens, use the original input value.
-                newValue = valueBeforeUpDown === null
-                  ? value
-                  : valueBeforeUpDown;
+                newValue =
+                  valueBeforeUpDown === null ? value : valueBeforeUpDown;
               } else {
                 newValue = this.getSuggestionValueByIndex(
                   newHighlightedSectionIndex,
