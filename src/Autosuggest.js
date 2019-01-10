14bfaaec5c24ed2184f2cdd7df4fa5c624829eb1
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2019-01-10 15:26:17 +0100

    Consume Enter event when selecting suggestion
    This is useful when this component is used with texareas.

--- src/Autosuggest.js
+++ src/Autosuggest.js
@@ -666,6 +666,7 @@ export default class Autosuggest extends Component {
             }
 
             if (highlightedSuggestion != null) {
+              event.preventDefault();
               const newValue = getSuggestionValue(highlightedSuggestion);
 
               this.maybeCallOnChange(event, newValue, 'enter');
