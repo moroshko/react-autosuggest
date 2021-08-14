296608fad6b9e7cbf64379c9854293dd72610831
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-14 13:51:25 +0800

    Delete demo directory

--- demo/src/components/utils/utils.js
+++
@@ -1,4 +0,0 @@
-// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_special_characters
-const escapeRegexCharacters = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-
-export { escapeRegexCharacters };
--- demo/src/index.js
+++
@@ -1,8 +0,0 @@
-import es6promise from 'es6-promise';
-import React from 'react';
-import { render } from 'react-dom';
-import App from 'App/App';
-
-es6promise.polyfill(); // Required, because `Promise` is undefined in IE.
-
-render(<App />, document.getElementById('demo'));
