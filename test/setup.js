549de1a9e5775519ed0992c60debc725f145c3a4
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-14 13:52:39 +0800

    Delete test directory

--- test/setup.js
+++
@@ -1,7 +0,0 @@
-import { JSDOM } from 'jsdom';
-
-const dom = new JSDOM(`<!doctype html><html><body></body></html>`);
-
-global.window = dom.window;
-global.document = dom.window.document;
-global.navigator = dom.window.navigator;
